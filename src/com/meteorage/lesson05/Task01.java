c30e697d138957961447d896c10491fb794555c2
Vologin_VV <vsevolod.vologin@gt-m.ru>
2023-03-15 12:38:57 +0500

    add fix into lesson 05

---
+++ src/com/meteorage/lesson05/Task01.java
@@ -0,0 +1,27 @@
+package com.meteorage.lesson05;
+
+public class Task01 {
+
+    public static void main(String[] args) {
+        Person[] persArray = new Person[5];
+
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivanov@mail.com", "+7-932-00-00-000", 10000, 30);
+        persArray[1] = new Person("Petrov Petr", "Engineer", "petrov@mail.com", "+79220000000", 11000, 39);
+        persArray[2] = new Person("Guseva Ksu", "Analytic", "guseva@mail.com", "+79120000000", 12000, 26);
+        persArray[3] = new Person("Sergova Anna", "Analytic", "sergova@mail.com", "+79130000000", 9000, 30);
+        persArray[4] = new Person("Vologin Seva", "Engineer 1C", "vologin@mail.com", "+79050000000", 15000, 35);
+
+        System.out.print("Список сотрудников старше 45: ");
+        if (Person.countPersOver45 > 0) {
+            System.out.println();
+            for (Person person : persArray) {
+                if (person.getAge() > 40) {
+                    System.out.println(person);
+                }
+            }
+        }
+        else {
+            System.out.println("Список пуст");
+        }
+    }
+}
